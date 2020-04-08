3ffe57645fef0a77fb792dcae17204b9d3d9aa0d
plebtech <nbmc@protonmail.com>
2020-04-08 16:55:30 -0500

    forming / commenting uri form.

--- src/server/api-test.ts
+++ src/server/api-test.ts
@@ -1,4 +1,15 @@
-
+// formed URL:
+// base url
+// operation-name: the API call, i.e. findCompleted Items
+// service-version: API version
+// security-appname: app id from ebay developers program
+// response-data-format: we want JSON data
+// rest-payload: required for URL request to separate payload from standard headers
+// keywords: (payload)
+// itemFilter(0) the first item filter, with a name:value pair
+// itemFilter(1) second item filter with name:value pair
+// itemFilter(2) third item filter with name:value pair
+// paginationInput: entries per page (how many to return)
 
 let ebaySearchCompletedURL: string =
     `https://svcs.ebay.com/services/search/FindingService/v1?
@@ -11,6 +22,7 @@ let ebaySearchCompletedURL: string =
     itemFilter(0).name=Condition
     &itemFilter(0).value=3000&
     itemFilter(1).name=FreeShippingOnly&
-    itemFilter(1).value=true&itemFilter(2).name=SoldItemsOnly&
+    itemFilter(1).value=true&
+    itemFilter(2).name=SoldItemsOnly&
     itemFilter(2).value=true&
     paginationInput.entriesPerPage=10`;
