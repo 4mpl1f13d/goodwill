21d696df5af721fb5008f21e1294040e2ad8020c
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-04-08 15:28:51 -0500

    Tremain: uploaded original screen design for screens 3 and 4

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Screen3 from './Components/Screen3';
+import Screen4 from './Components/Screen4';
 
 class App extends React.Component<IAppProps, IAppState> {
 	constructor(props: IAppProps) {
@@ -20,11 +23,25 @@ class App extends React.Component<IAppProps, IAppState> {
 
 	render() {
 		return (
-			<main className="container my-5">
-				{/* <h1 className="text-primary text-center">Hello {this.state.name}!</h1> */}
-				<h1>Howdy {this.state.name}!</h1>
-				<h2>Hoody Hoo!</h2>
-			</main>
+
+			<Router>
+				<React.Fragment>
+					<main className="container my-5">
+						{/* <h1 className="text-primary text-center">Hello {this.state.name}!</h1> */}
+						<h1>Howdy {this.state.name}!</h1>
+						<h2>Hoody Hoo!</h2>
+					</main>
+					<Link to="localhost:3000">Home</Link>
+					<Link className="text-center mx-2" to="/Screen3">Screen3</Link>
+					<Link to="/Screen4">Screen4</Link>
+
+					<Switch>
+						<Route exact path="localhost:3000" />
+						<Route exact path="/Screen3" component={Screen3} />
+						<Route exact path="/Screen4" component={Screen4} />
+					</Switch>
+				</React.Fragment>
+			</Router>
 		);
 	}
 }
