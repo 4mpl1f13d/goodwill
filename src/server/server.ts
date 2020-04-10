076bfde472be72029f378aa40b7e945660312a6c
plebtech <nbmc@protonmail.com>
2020-04-10 14:45:08 -0500

    commented out route.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,12 @@
+// apiRouter commented out. not needed unless we implement auth.
+
 import * as express from 'express';
-import apiRouter from './routes';
+// import apiRouter from './routes';
 
 const app = express();
 
 app.use(express.static('public'));
-app.use(apiRouter);
+// app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
-
-console.log(fullURL);
