0c6c8cf7d8a0f85c49de610d41a54fc50b0eee48
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-04-14 13:06:09 -0500

    mapped the item information to pre-determined card bodies

--- src/client/Components/Details.jsx
+++ src/client/Components/Details.jsx
@@ -1,7 +1,50 @@
 import React, { Component } from 'react';
 import {Card} from 'react-bootstrap';
 
-export class DetailsV2 extends Component {
+export class Details extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        itemStats: [
+          {
+            itemName: 'Sony Xperia l3123',
+            condition: 'Used',
+            appraisal: 300,
+          },
+          {
+            itemName: 'Fire 10 UX347',
+            condition: 'New',
+            appraisal: 150,
+          },
+          {
+            itemName: 'Fire 7 UM926',
+            condition: 'Used',
+            appraisal: 25,
+          },
+        ],
+        newItemName: '',
+        newCondition: '',
+        newAppraisal: Number
+      }
+    }
+
+    handleAuthorInput(e) {
+      this.setState({ newItemName: e.target.value });
+    }
+
+  handleMessageInput(e) {
+      this.setState({ newCondition: e.target.value })
+    }
+
+  handleClick() {
+      const newStats = {
+          itemName: this.state.newItemName,
+          condition: this.state.newCondition,
+          newAppraisal: this.state.newAppraisal
+      };
+      this.setState({ itemStats: [...this.state.itemStats, newStats] });
+    }
+
   render() {
     
     return (
@@ -19,23 +62,28 @@ export class DetailsV2 extends Component {
                 </div>
 
               <div className="col-12" style={{overflowX: 'scroll', overflowY: 'hidden', whiteSpace: 'nowrap'}}>
-                <div className="d-inline-block mx-2 my-2">
-                <Card border="success" style={{ width: '14rem' }}>
-                  <Card.Header>Item 1</Card.Header>
-                  <Card.Body>
-                    <Card.Title>Make and Model</Card.Title>
-                    <Card.Text>
-                      Condition: Used
-                      <div className="d-flex justify-content-center">
-                      <input className="form-control my-2" type="text" placeholder="Appraisal" style={{ width: '100px' }} readOnly />
-                      </div>
-                    </Card.Text>
-                    <button className="btn btn-danger btn-sm" style={{ width: '95px', height: '30px'}}><i className="fa fa-trash"></i> Delete </button>
-                  </Card.Body>
-                </Card>
-                </div>
+              {this.state.itemStats.map((item) => {
 
-                <div className="d-inline-block my-2">
+                return (
+                  <div className="d-inline-block mx-2 my-2" key={item.id}>
+                    <Card border="success" style={{ width: '14rem' }}>
+                      <Card.Header>Item 1</Card.Header>
+                      <Card.Body>
+                        <Card.Title>{item.itemName}</Card.Title>
+                        <Card.Text>
+                          {item.condition}
+                          <div className="d-flex justify-content-center">
+                            <input className="form-control my-2" type="text" placeholder="Appraisal" style={{ width: '100px' }} readOnly />
+                          </div>
+                        </Card.Text>
+                        <button className="btn btn-danger btn-sm" style={{ width: '95px', height: '30px' }}><i className="fa fa-trash"></i> Delete </button>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                )
+              })}
+
+                {/* <div className="d-inline-block my-2">
                 <Card border="warning" style={{ width: '14rem' }}>
                   <Card.Header>Item 2</Card.Header>
                   <Card.Body>
@@ -50,70 +98,7 @@ export class DetailsV2 extends Component {
                   </Card.Body>
                 </Card>
                 </div>
-
-                <div className="d-inline-block mx-2 my-2">
-                <Card border="info" style={{ width: '14rem' }}>
-                  <Card.Header>Item 3</Card.Header>
-                  <Card.Body>
-                    <Card.Title>Make and Model</Card.Title>
-                    <Card.Text>
-                      Condition: Used
-                      <div className="d-flex justify-content-center">
-                      <input className="form-control my-2" type="text" placeholder="Appraisal" style={{ width: '100px' }} readOnly />
-                      </div>
-                    </Card.Text>
-                    <button className="btn btn-danger btn-sm" style={{ width: '95px', height: '30px'}}><i className="fa fa-trash"></i> Delete </button>
-                  </Card.Body>
-                </Card>
-                </div>
-
-                <div className="d-inline-block mx-1 my-2">
-                <Card border="danger" style={{ width: '14rem' }}>
-                  <Card.Header>Item 4</Card.Header>
-                  <Card.Body>
-                    <Card.Title>Make and Model</Card.Title>
-                    <Card.Text>
-                      Condition: Used
-                      <div className="d-flex justify-content-center">
-                      <input className="form-control my-2" type="text" placeholder="Appraisal" style={{ width: '100px' }} readOnly />
-                      </div>
-                    </Card.Text>
-                    <button className="btn btn-danger btn-sm" style={{ width: '95px', height: '30px'}}><i className="fa fa-trash"></i> Delete </button>
-                  </Card.Body>
-                </Card>
-                </div>
-
-                <div className="d-inline-block mx-1 my-2">
-                <Card border="secondary" style={{ width: '14rem' }}>
-                  <Card.Header>Item 5</Card.Header>
-                  <Card.Body>
-                    <Card.Title>Make and Model</Card.Title>
-                    <Card.Text>
-                      Condition: Used
-                      <div className="d-flex justify-content-center">
-                      <input className="form-control my-2" type="text" placeholder="Appraisal" style={{ width: '100px' }} readOnly />
-                      </div>
-                    </Card.Text>
-                    <button className="btn btn-danger btn-sm" style={{ width: '95px', height: '30px'}}><i className="fa fa-trash"></i> Delete </button>
-                  </Card.Body>
-                </Card>
-                </div>
-
-                <div className="d-inline-block mx-2 my-2">
-                <Card border="success" style={{ width: '14rem' }}>
-                  <Card.Header>Item 6</Card.Header>
-                  <Card.Body>
-                    <Card.Title>Make and Model</Card.Title>
-                    <Card.Text>
-                      Condition: Used
-                      <div className="d-flex justify-content-center">
-                      <input className="form-control my-2" type="text" placeholder="Appraisal" style={{ width: '100px' }} readOnly />
-                      </div>
-                    </Card.Text>
-                    <button className="btn btn-danger btn-sm" style={{ width: '95px', height: '30px'}}><i className="fa fa-trash"></i> Delete </button>
-                  </Card.Body>
-                </Card>
-                </div>
+                 */}
 
               </div>
 
@@ -142,4 +127,4 @@ export class DetailsV2 extends Component {
   }
 }
 
-export default DetailsV2
+export default Details
